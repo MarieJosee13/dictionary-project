7f4676158b2cfd9348af94ade87c72175912bf68
MarieJosee13 <mariejosee1313@gmail.com>
2023-04-10 16:16:11 -0700

    added a new component and a loop

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,14 +1,27 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
   console.log(props.meaning);
   return (
     <div className="Meaning">
       <h4>{props.meaning.partOfSpeech}</h4>
-      <p>{props.meaning.definition}</p>
-      <p>
-        <em>{props.meaning.example}</em>
-      </p>
+      {props.meaning.definitions.map(function (definition, index) {
+        return (
+          <div key={index}>
+            <p>
+              <strong>Definition:</strong>
+              {definition.definition}
+              <br />
+              <em>{definition.example}</em>
+
+              <br />
+              <strong>Synonyms:</strong>
+              <Synonyms synonyms={definition.synonyms} />
+            </p>
+          </div>
+        );
+      })}
     </div>
   );
 }

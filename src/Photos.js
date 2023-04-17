0dcbdb89cc1598e0fbb11765689466a5c5cf28ff
MarieJosee13 <mariejosee1313@gmail.com>
2023-04-17 08:58:32 -0700

    created a link to the original photos used in this project

--- src/Photos.js
+++ src/Photos.js
@@ -3,14 +3,19 @@ import "./Photos.css";
 
 export default function Photos(props) {
   if (props.photos) {
-    console.log(props.photos);
     return (
       <section className="Photos">
         <div className="row">
           {props.photos.map(function (photo, index) {
             return (
               <div className="col-4" key={index}>
-                <img alt="" src={photo.src.landscape} className="img-fluid" />
+                <a href={photo.src.original} target="_blank" rel="noreferrer">
+                  <img
+                    alt="#"
+                    src={photo.src.landscape}
+                    className="img-fluid"
+                  />
+                </a>
               </div>
             );
           })}
